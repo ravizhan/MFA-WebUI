2424c049ef0a3084dba17147508df7bfa2e98d31
ravizhan <ravizhan@hotmail.com>
2026-01-26 12:53:59 +0800

    refactor(RightPanel): 移除未使用的对话框逻辑和时间格式化函数

--- front/src/components/RightPanel.vue
+++ front/src/components/RightPanel.vue
@@ -10,13 +10,12 @@
 <script setup lang="ts">
 import type { LogInst } from "naive-ui"
 import Clipboard from "clipboard"
-import { useMessage, useDialog } from "naive-ui"
+import { useMessage } from "naive-ui"
 import { sse } from "../script/sse"
 import { ref, onMounted, onUnmounted, watchEffect, nextTick } from "vue"
 import { useIndexStore } from "../stores"
 import { storeToRefs } from "pinia"
 
-const dialog = useDialog()
 const message = useMessage()
 const indexStore = useIndexStore()
 const log = storeToRefs(indexStore).RunningLog
@@ -26,51 +25,9 @@ btnCopy.on("success", () => {
   message.success("复制成功")
 })
 
-function is_now(date_string: string) {
-  function formatDate(date: Date) {
-    const year = date.getFullYear()
-    const month = String(date.getMonth() + 1).padStart(2, "0")
-    const day = String(date.getDate()).padStart(2, "0")
-    const hours = String(date.getHours()).padStart(2, "0")
-    const minutes = String(date.getMinutes()).padStart(2, "0")
-    const seconds = String(date.getSeconds()).padStart(2, "0")
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-  }
-  return date_string === formatDate(new Date())
-}
-
 const handleLog = (data: { message: string }) => {
   const msg = data.message
   indexStore.UpdateLog(msg)
-  if (msg.includes("请求接管") && is_now(msg.split(" ")[0] + " " + msg.split(" ")[1])) {
-    message.warning(msg)
-    new Notification("请求接管", {
-      body: msg + "\n" + "完成接管后请点击确定",
-    })
-    dialog.warning({
-      title: "请求接管",
-      content: msg + "\n" + "完成接管后请点击确定",
-      positiveText: "确定",
-      closable: false,
-      maskClosable: false,
-      onPositiveClick: () => {
-        fetch("/api/continue", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data["status"] === "success") {
-              message.success("正在恢复任务")
-            } else {
-              message.error(data["message"])
-            }
-          })
-      },
-    })
-  }
 }
 sse.addEventListener('log', handleLog)
 
