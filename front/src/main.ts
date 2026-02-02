97501509a65805e816c0e109a248cfc45da5ff28
ravizhan <ravizhan@hotmail.com>
2026-02-03 03:09:10 +0800

    feat(i18n): 添加国际化支持，支持中英文语言切换

--- front/src/main.ts
+++ front/src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue"
 import router from "./router"
 import { createPinia } from "pinia"
 import "virtual:uno.css"
-import i18n from "./lib/i18n"
+import i18n from "./libs/i18n"
 
 const app = createApp(App)
 const pinia = createPinia()
