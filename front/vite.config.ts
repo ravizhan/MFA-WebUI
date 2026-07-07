fe6017c2bc4c43b561ff5e0222b9ade1d9846645
ravizhan <ravizhan@hotmail.com>
2026-07-07 15:05:46 +0800

    chore(build): 移除手动分块逻辑

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -19,34 +19,12 @@ export default defineConfig({
   ],
   build: {
     outDir: "../page",
-    cssMinify: "lightningcss",
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (
-            id.includes("/node_modules/vue/") ||
-            id.includes("/node_modules/vue-router/") ||
-            id.includes("/node_modules/pinia/") ||
-            id.includes("/node_modules/vue-i18n/")
-          ) {
-            return "vendor-vue"
-          }
-          if (id.includes("/node_modules/marked/") || id.includes("/node_modules/dompurify/")) {
-            return "vendor-markdown"
-          }
-          if (id.includes("/node_modules/vue-draggable-plus/")) {
-            return "vendor-drag"
-          }
-        },
-      },
-    },
   },
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:5566",
         changeOrigin: true,
-        ws: true,
       },
     },
   },
